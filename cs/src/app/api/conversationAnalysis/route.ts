05a64d94168f39bcca22c7f619ceed33605a5a75
min <cmt14033@nitech.jp>
2024-10-26 16:38:23 +0900

    GETメソッド削除

--- cs/src/app/api/conversationAnalysis/route.ts
+++ cs/src/app/api/conversationAnalysis/route.ts
@@ -13,49 +13,6 @@ const schema = z.object({
 	),
 });
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
-	// サンプルの会話メッセージを作成
-	const messages = [
-		{ text: "最近、読書にはまっているんですよ。", role: "assistant" },
-		{ text: "そうなんですね。", role: "user" },
-	];
-
-
-
-	try {
-
-		// プロンプトテンプレートを設定
-		// 	const promptTemplate = new PromptTemplate({
-		// 		template: `{conversation}\n\n{additionalPrompt}\nassistant:`,
-		// 		inputVariables: ["conversation", "additionalPrompt"],
-		// 	});
-
-		// 	// 会話チェーンを作成
-		// 	const conversationChain = new ConversationChain({
-		// 		llm: llm,
-		// 		prompt: promptTemplate,
-		// 	});
-
-		// 	// AIモデルにプロンプトを送信し、応答を取得
-		// 	const response = await conversationChain.call({
-		// 		conversation: conversationText,
-		// 		additionalPrompt: additionalPrompt, // 追加の文章を渡す
-		// 	});
-
-		// 	const message = response?.text || "エラーが発生しました。";
-
-		// 	// 応答メッセージを返す
-		// 	return NextResponse.json({ message });
-		// } catch (error) {
-		// 	// エラー処理
-		// 	console.error("LangChainを通してAIから応答を取得する際にエラーが発生しました:", error);
-		// 	return NextResponse.json({ message: "AIリクエストに失敗しました。" });
-		// }
-	} catch (error) {
-	}
-
-};
-
 export const POST = async (req: NextRequest, res: NextResponse) => {
 	const body = await req.json();
 	const { messages } = schema.parse(body);
